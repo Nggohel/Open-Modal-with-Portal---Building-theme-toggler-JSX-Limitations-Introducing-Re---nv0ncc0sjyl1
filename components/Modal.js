127780fabbf998a5cd6b4948d129e7fe41aac796
Nggohel <gohelnidhi15898@gmail.com>
2023-12-26 12:15:26 +0530

    add

--- components/Modal.js
+++ components/Modal.js
@@ -1,14 +1,20 @@
 // ./components/Modal.js
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className='modal-overlay' onClick={onClose}>
-      <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }}>
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div
+          style={{
+            backgroundColor: "white",
+            padding: "2rem",
+            position: "relative",
+          }}
+        >
           {children}
           <button onClick={onClose}>Close Modal</button>
         </div>
@@ -19,3 +25,4 @@ const Modal = ({ children, isOpen, onClose }) => {
 };
 
 export default Modal;
+
