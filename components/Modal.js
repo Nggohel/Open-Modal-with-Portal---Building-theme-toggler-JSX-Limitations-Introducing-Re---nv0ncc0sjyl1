f7f3d8f24fd5ccc3e845919ef76b2544a7f2529a
Nggohel <gohelnidhi15898@gmail.com>
2023-12-26 12:10:52 +0530

    add logic

--- components/Modal.js
+++ components/Modal.js
@@ -1,18 +1,21 @@
-
+// ./components/Modal.js
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
- //add code here
-    <div className='modal'>
-      
-      <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >// This prevents the modal from closing when clicking inside
-   //add code here
+  return ReactDOM.createPortal(
+    <div className='modal-overlay' onClick={onClose}>
+      <div className='modal' onClick={(e) => e.stopPropagation()}>
+        <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }}>
+          {children}
+          <button onClick={onClose}>Close Modal</button>
+        </div>
       </div>
     </div>,
     document.body
-
+  );
 };
 
 export default Modal;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
-import { useState } from 'react';
-import Modal from '../components/Modal';
+// ./pages/index.js
+import React, { useState } from "react";
+import Modal from "../components/Modal";
 
 const HomePage = () => {
-  
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div>
-      <button>Open Modal</button>
+      <button onClick={openModal}>Open Modal</button>
 
-      <Modal >
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
         <h2>This is a Modal</h2>
         <p>Using React Portals with Next.js</p>
-        <button >Close Modal</button>
       </Modal>
     </div>
   );
